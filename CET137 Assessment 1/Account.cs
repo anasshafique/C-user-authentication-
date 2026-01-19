5be6b6392c64ff8dff2de3f2299732ed229d5216
Anas Shafique <anasshafique30@gmail.com>
2026-01-19 09:56:28 +0000

    Add files via upload

---
+++ CET137 Assessment 1/Account.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CET137_Assessment_1
+{
+    internal class Account
+    {
+
+        private String susername;
+        private String spassword;
+
+        public void Registeration()  // Asks the user to enter username.
+        {
+            Console.WriteLine("********** Register your account *********");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter your username and password");
+
+            Console.WriteLine();
+
+            Console.Write("Username: ");
+
+            susername = Console.ReadLine(); 
+
+        }
+
+        public void UsernameValidation() // Validates the Username.
+        {
+            while (true) 
+
+            {
+
+                if (File.Exists(susername + ".TXT")) // Check if the file exists or not.
+
+                {
+
+                    Console.WriteLine("The username already exists. Enter a different username.");
+                    Console.Write("Username: ");
+                    susername = Console.ReadLine();
+
+                }
+
+                //else if (susername != susername + ".TXT")
+                //{
+                //    break;
+                //}
+
+                else
+
+                {
+
+                    break;
+
+                }
+
+            }
+
+            using (StreamWriter sw = new StreamWriter(susername + ".TXT", true)) // Used to create a file with the name of the file being the username of account.
+
+            {
+
+                sw.WriteLine("username = " + susername);
+
+            }
+        }
+
+        public void PasswordValidation() // Validates the password.
+        {
+
+            Console.WriteLine("Enter atleast 10-character password");
+
+            Console.WriteLine();
+
+            Console.Write("Password: ");
+            spassword = Console.ReadLine();
+
+            while (true) 
+
+            {
+
+                if (spassword.Length >= 10) // Verifies that the password is atleast 10 characters or more.
+
+                {
+                    break;
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("The password should be atleast 10-characters or more");
+
+                    Console.WriteLine();
+
+                    Console.Write("Password: ");
+                    spassword = Console.ReadLine(); 
+
+                }
+
+            }
+
+            String[] sostusedpass = File.ReadAllLines("Password.txt"); // Reads all of the lines in the password text file and stores that in amostusedpass array of datatype string.
+
+            while (true) 
+
+            {
+
+                if (sostusedpass.Contains(spassword)) // Checks if the password file contains a similer password the the user entered or not.
+
+                {
+
+                    Console.WriteLine("The Password is too common use a different ones");
+
+                    Console.WriteLine();
+
+                    Console.Write("Password = ");
+                    spassword = Console.ReadLine();
+                }
+
+                else
+
+                {
+
+                    break; 
+
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(susername + ".TXT", true)) // Writes the password in the file being the username of the account.
+
+            {
+
+                sw.WriteLine("Password = " + spassword);
+
+            }
+
+            Console.Clear();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Account registered successfully!");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue");
+
+            Console.ReadKey();
+
+            Console.Clear();
+        }
+
+        public void Login() // User login to account.
+        {
+            Console.WriteLine("********** Login to your account *********");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter your username and password to login");
+
+            Console.WriteLine();
+
+            Console.Write("Username: ");
+            susername = Console.ReadLine();
+            Console.Write("Password: ");
+            spassword = Console.ReadLine();
+
+            if (File.Exists(susername + ".TXT")) // Checks if the file being the username of the account exists or not.
+
+            {
+
+                String[] readpass = File.ReadAllLines(susername + ".TXT"); // Reads all of the lines in the text file being the username of the account and stores that in readpass array of datatype string.
+
+                if (readpass[1].Contains(spassword)) // Check if the second line of the textfile contains the password or not.
+
+                {
+
+
+
+                    Console.Clear();
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("You have successfully logged in!");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press any key to continue");
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+
+                }
+
+                else
+
+                {
+
+                    Console.Clear();
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Incorrect Password");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press any key to continue");
+
+                    Console.ReadKey();
+
+                    Console.Clear();
+
+                }
+
+
+            }
+
+            else
+
+            {
+
+                Console.Clear();
+
+                Console.WriteLine();
+
+                Console.WriteLine("Incorrect Username");
+
+                Console.WriteLine();
+
+                Console.WriteLine("Press any key to continue");
+
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+
+        }
+    }
+
+
+
+
+}
+
---
+++ CET137 Assessment 1/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CET137_Assessment_1
+{
+    internal class Program
+    {
+        static void MenuOptions() // Shows menu options to the user.
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("                                            Welcome to the the Application");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Given below are some menu choices you would want to select from:");
+
+            Console.WriteLine();
+
+            Console.WriteLine("To Register your account press '1'");
+            Console.WriteLine("To Login press                 '2'");
+            Console.WriteLine("To quit program press          '0'");
+
+            Console.WriteLine();
+        }
+
+        static int MenuVerification() // Validates the menu choice.
+        {
+            int ichoice;
+
+            while (true) // To keep the program running.
+
+            {
+                Console.Write("Choice: "); if (!int.TryParse(Console.ReadLine(), out ichoice)) // To avoid errors and make sure the user input an integer.
+
+                {
+
+                    Console.WriteLine("Invalid input. Please enter a valid input");
+
+                }
+
+                else if (ichoice < 0 || ichoice > 2) // Verifies the range to avoid errors.
+
+                {
+
+                    Console.WriteLine("Please enter a valid choice");
+
+                }
+
+                else
+
+                {
+
+                    break; // Breaks the loop if nothing above is true.
+
+                }
+
+            }
+
+            Console.Clear();
+
+            return ichoice; // The value stored in ichoice will be sent to main function.
+        }
+
+            static void Main(string[] args)
+
+            {
+
+            int ichoice;
+
+            Account account = new Account(); // Created the instance account
+
+            while (true)
+            {
+                MenuOptions();
+
+                ichoice = MenuVerification();
+
+                if (ichoice == 0)
+                {
+                    Console.WriteLine("Program Existed");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Thank you for using the Application, Goodbye!");
+                    break;
+
+                }
+
+                switch (ichoice)
+                {
+                    case 1: //Classes are being called.
+
+                        account.Registeration();
+                        account.UsernameValidation();
+                        account.PasswordValidation();
+
+                        break;
+
+                    case 2:
+
+                        account.Login();
+
+                        break;
+
+                }
+            }
+
+            Console.ReadKey();
+        } 
+    }  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if (susername != susername + ".TXT")
+//{
+//    break;
+//}
+//using (StreamReader sr = new StreamReader("DONATIONS.TXT"))
+//{
+
+//    while (sr.Peek() != -1) // while more data to read
+//    {
+//        amostusedpass = sr.ToStri
+//        Console.WriteLine(sName);
+//        dmoney = Convert.ToDouble(sr.ReadLine());
+//        Console.WriteLine(dmoney);
+
+//        //string donationLine = sr.ReadLine(); // Read donation line
+//        //Console.WriteLine(donationLine); // Display donation line as-is
+
+
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+//if(spassword == amostusedpass)
+//{
+
+//}
+//readpass.Length > 1
+
